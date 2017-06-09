922562a479151ed6329e13f1cc0900eb3526d724
snclucas <snclucas@gmail.com>
2017-06-08 21:14:14 -0400

    Fixed JWL

--- UserDocumentResource.py
+++ UserDocumentResource.py
@@ -11,25 +11,17 @@ class UserDocumentResource:
         self.database_manager = RethinkDBDatabaseManager.RethinkDBDatabaseManager()
 
     def on_get(self, req, resp, table=None, doc_id=None):
-        # Return note for particular ID
-        if doc_id:
-            resp.body = self.database_manager.get_one(table, doc_id)
+        [status, jwt_result] = self.check_jwt(req.headers)
+        if status is True:
+            # Return note for particular ID
+            if doc_id:
+                resp.body = self.database_manager.get_one(table, doc_id)
+            else:
+                resp.body = self.database_manager.get_all(table)
         else:
-            resp.body = self.database_manager.get_all(table)
+            resp.body = jwt_result
 
-        if 'AUTHENTICATION' in req.headers:
-            token = req.headers['AUTHENTICATION'].split("Bearer")[1].replace(" ", "")
 
-            jwt_result = ""
-            try:
-                jwt_result = jwt.decode(token, 'tree', algorithm=['HS256'])
-            except jwt.ExpiredSignatureError:
-                resp.body = '{"Token expired"}'
-            except jwt.InvalidTokenError:
-                resp.body = '{"Invalid token"}'
-            print(self.check_jwt(req.headers))
-        else:
-            resp.body = '{"No authentication token supplied"}'
 
     def on_post(self, req, resp, table=None):
         # If table does not exist, create it
@@ -42,7 +34,7 @@ class UserDocumentResource:
         try:
             result = json.loads(raw_json)
             sid = self.database_manager.save(result, table)
-            resp.body = '{"message": "Successfully inserted.", "id": '+sid+'}'
+            resp.body = '{"message": "Successfully inserted.", "id": "'+sid+'"}'
         except ValueError:
             raise falcon.HTTPError(falcon.HTTP_400, 'Invalid JSON', 'Could not decode the request body. The ''JSON was incorrect.')
 
@@ -51,13 +43,11 @@ class UserDocumentResource:
             token = headers['AUTHENTICATION'].split("Bearer")[1].replace(" ", "")
 
             try:
-                jwt_result = jwt.decode(token, 'tree', algorithm=['HS256'])
-                res = '{"status": "OK"}'
+                jwt_result = jwt.decode(token, 'secret', algorithm=['HS256'])
+                res = json.loads('{"status": "OK"}')
                 res['jwt'] = json.dumps(jwt_result)
-                return res
-            except jwt.ExpiredSignatureError:
-                return '{"status": "Fail", "message": "Token expired"}'
+                return [True, res]
             except jwt.InvalidTokenError:
-                return '{"status": "Fail", "message": "Invalid token"}'
+                return [False, '{"status": "Fail", "message": "Invalid token"}']
         else:
-            return '{"status": "Fail", "message": "No authentication token supplied"}'
+            return [False, '{"status": "Fail", "message": "No authentication token supplied"}']
