8131cb3baaffd4b61cd504b9c48071b69f2a1c9b
snclucas <snclucas@gmail.com>
2017-07-07 21:39:02 -0400

    Fixed tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,57 @@
-
 from falcon import testing
-from urllib.parse import urlparse
-import http.client
+import json
+import requests
+
+import app
 
 
 class StashyTestCase(testing.TestCase):
+
+    header_with_token = {"Content-Type": "application/json", "Authorization": "Bearer 0d24a98c5544578cedd7055d2a6eeb4d"}
+    test_url = 'https://stashy.io/api'
+
     def setUp(self):
         super(StashyTestCase, self).setUp()
+        self.app = app.api
 
 
-class TestStashy(StashyTestCase):
-    def test_no_token(self):
-        url = 'http://localhost:8000/d/test/docs'
+class TestStashyAuthorization(StashyTestCase):
+    def test_get_no_token(self):
         headers = {"Content-Type": "application/json"}
-        domain = urlparse(url).netloc
-        connection = http.client.HTTPConnection(domain)
-        connection.request("GET", url, headers=headers)
-        response = connection.getresponse()
-        self.assertEqual(response.read().decode(), '{"status": "Fail", "message": "No authentication token supplied"}')
+        result = self.simulate_request(method='GET', path='/d/test/docs', headers=headers, protocol='http')
+        self.assertEqual(result.json, {'status': 'fail', 'message': 'No authentication token supplied'})
 
-    def test_invalid_token(self):
-        url = 'http://localhost:8000/d/test/docs'
-        headers = {"Content-Type": "application/json", "Authorization": "Bearer 3453"}
-        domain = urlparse(url).netloc
-        connection = http.client.HTTPConnection(domain)
-        connection.request("GET", url, headers=headers)
-        response = connection.getresponse()
-        self.assertEqual(response.read().decode(), '{"status": "Fail", "message": "Invalid token"}')
+    def test_get_invalid_token(self):
+        headers = {"Content-Type": "application/json", "Authorization": "Bearer 0d24a"}
+        result = self.simulate_request(method='GET', path='/d/test/docs', headers=headers, protocol='http')
+        self.assertEqual(result.json, {"status": "fail", "message": "No user with that token"})
+
+    def test_post_get_valid_token(self):
+
+        result = self.simulate_request(method='POST', path='/d/test/docs',
+                                       headers=self.header_with_token, protocol='http',
+                                       body=json.dumps({"spam": "1", "eggs": "2"}))
+
+        json_data = result.json[0]
+
+        self.assertIn('id', json_data)
+
+        newdoc_id = json_data['id']
+
+        self.assertIn('spam', json_data)
+        self.assertEqual(json_data['spam'], "1")
+
+        self.assertIn('eggs', json_data)
+        self.assertEqual(json_data['eggs'], "2")
+
+        result = self.simulate_request(method='GET', path='/d/test/docs/' + newdoc_id,
+                                       headers=self.header_with_token, protocol='http')
+
+        json_data_get = result.json
+
+        self.assertIn('spam', json_data_get)
+        self.assertEqual(json_data_get['spam'], "1")
+
+        self.assertIn('eggs', json_data_get)
+        self.assertEqual(json_data_get['eggs'], "2")
+
